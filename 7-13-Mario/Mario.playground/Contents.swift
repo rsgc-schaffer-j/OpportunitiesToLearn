e5e54a54e3bc3cd4625c36f80555bab8f7cdc5fa
Jasper <Jasper@macbook-pro-0663-jasper-schaffer.local>
2016-12-05 14:22:07 -0500

    i added comments

--- 7-13-Mario/Mario.playground/Contents.swift
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -12,16 +12,16 @@
 
 // Add your code below
 // your code here
-func levelCost(heights heights: [Int], maxJump: Int)->Int{
+func levelCost(heights heights: [Int], maxJump: Int)->Int{      //defining the function with the necessary arguments
     var total = 0
     var x = 1
     var y = 0
-    for x in  1...heights.count-1{
+    for x in  1...heights.count-1{      //-1 so it does not leave the array bounds
         y = (heights[x]-heights[x-1])
-        if y<0{
+        if y<0{         //finding the absolute value
             y *= -1
         }
-        if heights[x]==heights[x-1]{
+        if heights[x]==heights[x-1]{        //if statments deciding what type of motion occured
             total+=1
         }else if y<=maxJump{
             total+=y*2
