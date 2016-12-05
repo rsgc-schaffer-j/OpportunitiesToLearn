0eb92ad28587285675c3d14024ab4f670d8debe7
rgordonatrsgc-student <student@russellgordon.ca>
2016-12-05 06:02:32 -0500

    Added Mario boilerplate text

---
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -0,0 +1,13 @@
+/*:
+ # Mario
+ 
+ Add your Mario solution (however far you got) to the space below.
+ 
+ Then, commit your work.
+ 
+ Finally, push your work to the remote origin for your repository on GitHub.com.
+ 
+ We will go through this together.
+ */
+
+// Add your code below...
