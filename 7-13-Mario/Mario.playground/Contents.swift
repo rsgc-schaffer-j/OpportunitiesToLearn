77f202ebb267bd98b111a086f68d6f3aea399476
Jasper <Jasper@macbook-pro-0663-jasper-schaffer.local>
2016-12-05 08:45:54 -0500

    my attempt at the mario challenge

--- 7-13-Mario/Mario.playground/Contents.swift
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -11,3 +11,24 @@
  */
 
 // Add your code below
+// your code here
+func levelCost(heights heights: [Int], maxJump: Int)->Int{
+    var total = 0
+    var x = 1
+    var y = 0
+    while x < heights.count{
+        y = (heights[x]-heights[x-1])
+        if y<0{
+            y *= -1
+        }
+        if heights[x]==heights[x-1]{
+            total+=1
+        }else if y<=maxJump{
+            total+=y*2
+        }else {
+            return -1
+        }
+        x+=1
+    }
+    return total
+}
